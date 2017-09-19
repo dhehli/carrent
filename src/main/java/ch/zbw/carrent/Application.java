fa78215ae1af228995c0032bbb0c3d223bde7f50
dhehli <dh@netlive.ch>
2017-09-19 09:58:01 +0200

    add ssl param change application.java

--- src/main/java/ch/zbw/carrent/Application.java
+++ src/main/java/ch/zbw/carrent/Application.java
@@ -1,20 +1,11 @@
 package ch.zbw.carrent;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.TestPropertySource;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.internal.org.objectweb.asm.TypeReference;
-import ch.zbw.model.*;
 
 import java.util.Arrays;
-import java.util.List;
-import jdk.internal.org.objectweb.asm.*;
+
 
 
 @TestPropertySource(properties = "debug=true")
@@ -24,16 +15,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	
-	@Bean
-	CommandLineRunner init(KundeRepository accountRepository) {
-		return (evt) -> Arrays.asList(
-				"hans, wurst".split(","))
-				.forEach(
-						a -> {
-							//Kunde k = accountRepository.save(new Kunde(a, "test", "street1", 9545, "Wängi"));
-						});
-	}
-
 }
